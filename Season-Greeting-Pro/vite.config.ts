b050be8e966964b6775aca2cd9a7c1d2ca4553f6
Adebayo Oladayo <matthewadebayo392@gmail.com>
2024-09-11 20:44:25 +0100

    workpls

--- Season-Greeting-Pro/vite.config.ts
+++ Season-Greeting-Pro/vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Season-Greeting-Pro/', // The name of your GitHub repository
+  plugins: [react()]
 })
